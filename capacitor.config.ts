8121b835103cd4cd6372ecf8ea4112a3f573765d
Judech <edujkie.123@gmail.com>
2025-08-22 17:06:30 +0800

    WIP: local changes before pulling latest from origin

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.judech.myapp',
+  appName: 'QMAZ APP',
+  webDir: 'dist'
+};
+
+export default config;
